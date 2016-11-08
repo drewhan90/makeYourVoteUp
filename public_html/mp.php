779e9332b84a56410ededc470ae5b74cfe7353ad
takfukuda917 <tak.fukuda917@gmail.com>
2016-11-08 15:05:01 -0800

    MP - Debug

--- public_html/mp.php
+++ public_html/mp.php
@@ -1,10 +1,22 @@
-<?php include 'partials/header.php';?>
-    <!-- PAGE TITLE SECTION-->
-    <section class="title-section">
-        <h1>who's my mp</h1>
-    </section>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
 
-    <main onload="loadXMLDoc('', '')" class="mp-main">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Make Your Vote Up</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/mike-footer.app.css">
+</head>
+
+<body onload="loadXMLDoc('', '')">
+
+<?php include 'partials/mike-header.php';?>
+
+    <main class="mp-main">
         <!-- MAP SECTION-->
         <section class="map-section">
             <div class="map-svg">
@@ -14,7 +26,9 @@
 
         <!-- PARTY DESCRIPTION SECTION -->
         <section class="mp-list-section">
-            <table id="mp"></table>
+            <div class="row">
+                <table class="table-spacing small-12 medium-12 large-12 columns" id="mp"></table>
+            </div>
         </section>
     </main>
 
