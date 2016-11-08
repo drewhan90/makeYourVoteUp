cb812818bf00cdbd7ad90305a844d58a15401c3e
Andrew Yeonsoo Han <drewhan90@gmail.com>
2016-11-08 00:01:51 -0800

    header update

--- public_html/partials/header.php
+++ public_html/partials/header.php
@@ -23,8 +23,15 @@
 		</div>
 		<div id="responsive-menu">
 			<div class="top-bar-right">
-				<ul class="menu">
-					<li><a href="party.php">Party Info</a></li>
+				<ul class="dropdown menu" data-dropdown-menu>
+					<li><a href="">Party Info</a>
+						<ul class="menu vertical">
+							<li><a href="liberal.php">Liberal Party</a></li>
+							<li><a href="conservative.php">Conservative Party</a></li>
+							<li><a href="green.php">Green Party</a></li>
+							<li><a href="ndp.php">NDP Party</a></li>
+						</ul>
+					</li>
 					<li><a href="mp.php">Who's My Mp</a></li>
 					<li><a class="primary-btn" href="compare.php">Compare Parties</a></li>
 				</ul>
