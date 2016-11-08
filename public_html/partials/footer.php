8577215346bcd4913c663053532c57891627b70c
takfukuda917 <tak.fukuda917@gmail.com>
2016-11-08 10:23:05 -0800

    Icon and css fixed

--- public_html/partials/footer.php
+++ public_html/partials/footer.php
@@ -1,28 +1,36 @@
 <footer class="page-footer row">
     <div class="large-6 columns">
         <ul>
-            <li><h2 class="footer-header">SIGN UP FOR THE NEWSLETTER</h2></li>
+            <li>
+                <h2 class="footer-header">SIGN UP FOR THE NEWSLETTER</h2></li>
             <li class="footer-spacing">
-            	<div>
-               		<input class="submit-border inline-block submit-input left" type="email" placeholder="Email Address">
-               		<button class="left" type="submit">
-                		<img class="submit-border border-padding inline-block" class="inline" src="img/icon/icon-submit.png" alt="click to submit email">
-                	</button>
+                <div>
+                    <input class="submit-border inline-block submit-input left" type="email" placeholder="Email Address">
+                    <button class="left" type="submit">
+                        <img class="submit-border border-padding inline-block" class="inline" src="img/icon/icon-submit.png" alt="click to submit email">
+                    </button>
                 </div>
             </li>
-            <li><h2 class="footer-header">&copy;2016 MAKE YOUR VOTE UP</h2></li>
+            <li>
+                <h2 class="footer-header">&copy;2016 MAKE YOUR VOTE UP</h2></li>
         </ul>
     </div>
     <div class="large-6 columns">
         <ul>
-            <li><h2 class="footer-header">CONTACT US</h2></li>
-            <li class="footer-spacing"><p class="footer-body">makeyourvoteup@vanarts.com</p></li>
-            <li><p class="footer-body">+1 123-456-7890</p></li>
-            <li class="footer-second-row"><h2 class="footer-header">FOLLOW US</h2></li>
+            <li>
+                <h2 class="footer-header">CONTACT US</h2></li>
             <li class="footer-spacing">
-                <a  href="www.facebook.com"><img src="img/icon/icon-fb" alt="facebook icon"></a>
-                <a class="social-spacing" href="www.instagram.com"><img src="img/icon/icon-insta" alt="instagram icon"></a>
-                <a class="social-spacing" href="www.twitter.com"><img src="img/icon/icon-twit" alt="twitter icon"></a>
+                <p class="footer-body">makeyourvoteup@vanarts.com</p>
+            </li>
+            <li>
+                <p class="footer-body">+1 123-456-7890</p>
+            </li>
+            <li class="footer-second-row">
+                <h2 class="footer-header">FOLLOW US</h2></li>
+            <li class="footer-spacing">
+                <a href="www.facebook.com"><img src="img/icon/icon-fb.png" alt="facebook icon"></a>
+                <a class="social-spacing" href="www.instagram.com"><img src="img/icon/icon-insta.png" alt="instagram icon"></a>
+                <a class="social-spacing" href="www.twitter.com"><img src="img/icon/icon-twit.png" alt="twitter icon"></a>
                 <a class="jump-to-top right" href="#"><img src="img/icon/up-arrow.png" alt="click to jump to top of page"></a>
             </li>
         </ul>
